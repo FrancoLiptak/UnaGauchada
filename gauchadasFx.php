76823fab7f2a52be8d62c46957b495a40ac69053
SebaRaimondi <sebastianraimondi@gmail.com>
2017-05-26 17:16:57 -0300

    hotfix

--- gauchadasFx.php
+++ gauchadasFx.php
@@ -39,8 +39,13 @@ function showGauchadaForAll($gauchada)
             <td><?php echo $cate['name']; ?></td>
             <td><?php echo $city['name']; ?></td>
             <td><?php echo $gauchada['title']; ?></td>
-            <td><?php echo date_diff(date_create($gauchada['expiration']), date_create($hoy))->format('%a') ?></td>
+            <td><?php echo date_diff(date_create($gauchada['expiration']), date_create($hoy))->format('%a'); ?></td>
             <td><a class="details" href="detalle.php?idGauchadas=<?php echo $gauchada['idGauchadas']; ?>">&raquo; Ver detalle</a>
 
-    <img src="<?php echo $gauchada['image'] ?>" />
+    <img src="<?php echo $gauchada['image']; ?>" />
+
+            </td> 
+         </tr> 
+    <?php  
+    } 
 ?>
