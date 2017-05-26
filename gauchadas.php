13a5e4b8a9ffd88f09cc78e656e47e47c2448b9d
SebaRaimondi <sebastianraimondi@gmail.com>
2017-05-25 23:40:23 -0300

    Eliminado un echo de debuggeo de gauchadas.php

--- gauchadas.php
+++ gauchadas.php
@@ -8,57 +8,59 @@
   <br>
   <br>
   <br>
-  <?php 
-  include_once 'validate.php';
-  include_once 'gauchadasFx.php';
-  $anterior = false;
-  $siguiente = false;
+    <?php
+    include_once 'validate.php';
+    include_once 'gauchadasFx.php';
+    $anterior = false;
+    $siguiente = false;
 
-  if(isset($_POST['first'])){
-    $first = $_POST['first'];
-  }
-  else {
-    $first = 0;
-  }
+    if (isset($_POST['first'])) {
+        $first = $_POST['first'];
+    } else {
+        $first = 0;
+    }
 
-  if (!$first == 0) {
-    $anterior = true;
-  }
+    if (!$first == 0) {
+        $anterior = true;
+    }
 
-  $hoy = date("Y-m-d");
-  $condition = "";
-  $gauchadas = getGauchadas(10, $first);
-  $i = $gauchadas->num_rows;
-  echo $i;
-  switch ($i) {
-    case 0:
-      $_SESSION['msg'] = "No se encontraron resultados.";
-      break;
-    case 10:
-      $siguiente = true;
-      $i = 9;
-      break;
-  }
-  while ($i > 0) {
-    showGauchadaForAll($gauchadas->fetch_assoc()); ?>
-  <br>
-  <br>
-  <br>
-  <?php
-    $i--;
-  }  
-$prev = $first - 9;
-$next = $first + 9;
-  ?>  
+    $hoy = date("Y-m-d");
+    $condition = "";
+    $gauchadas = getGauchadas(10, $first);
+    $i = $gauchadas->num_rows;
+    switch ($i) {
+        case 0:
+            $_SESSION['msg'] = "No se encontraron resultados.";
+            break;
+        case 10:
+            $siguiente = true;
+            $i = 9;
+            break;
+    }
+    while ($i > 0) {
+        showGauchadaForAll($gauchadas->fetch_assoc()); ?>
+    <br>
+    <br>
+    <br>
+        <?php
+        $i--;
+    }
+    $prev = $first - 9;
+    $next = $first + 9;
+    ?>  
 
 <form action="gauchadas.php" method="post">
-    <input type="submit" name='anterior' id='anterior' value="anterior" <?php if (!$anterior) {echo "disabled";} ?> />
+    <input type="submit" name='anterior' id='anterior' value="anterior" <?php if (!$anterior) {
+        echo "disabled";
+} ?> />
     <input type="numer" name="first" id="first" value=<?php echo $prev;?> hidden />
 </form>
 <form action="gauchadas.php" method="post">
-    <input type="submit" name='proximo' id='proximo' value="proximo"  <?php if (!$siguiente) {echo "disabled";} ?> />
+    <input type="submit" name='proximo' id='proximo' value="proximo"  <?php if (!$siguiente) {
+        echo "disabled";
+} ?> />
     <input type="numer" name="first" id="first" value=<?php echo $next;?> hidden />
 </form>
 
 
-  <?php Include("footer.html");?>
+    <?php include("footer.html");?>
