465fd95328fbc0892183d33fba5a3aa318737d11
Camila <onofricamila2@gmail.com>
2017-05-26 00:30:46 -0300

    se corrigio el form del publicar

--- publicar.php
+++ publicar.php
@@ -37,28 +37,30 @@
         <form enctype="multipart/form-data" class="col-md-4 col-md-offset-4" action="procesarPublicar.php" method="post" target="_self" accept-charset="UTF-8" autocomplete="on" name="publucar_form" onsubmit="return validateFormPublicar()">
             <div class="form-group">
               <label for="title"><span class="glyphicon glyphicon-bookmark"></span> Título:</label>
-              <input type="text" class="form-control" id="title" name="title">
+              <input type="text" class="form-control" id="title" name="title" placeholder="Titulo">
             </div>
             <div class="form-group">
               <label for="description"><span class="glyphicon glyphicon-bookmark"></span> Descripción:</label>
-              <input type="text-area" class="form-control" id="description" name="description">
+              <textarea class="form-control" id="description" name="description" placeholder="Descripcion"></textarea>
             </div>
             <div class="form-group">
               <label for="expiration"><span class="glyphicon glyphicon-bookmark"></span> Fecha limite:</label>
               <input type="date" class="form-control" id="expiration" name="expiration" min="<?php echo date('Y-m-d');?>">
             </div>
-            <div class="form-group">
-              <label for="category"><span class="glyphicon glyphicon-bookmark"></span> Categoria:</label>
-              <select class="form-control" id="category" name="category">
-                <?php selectCates(); ?>
-            </select>
-            </div>
-            <div class="form-group">
-              <label for="city"><span class="glyphicon glyphicon-bookmark"></span> Ciudad:</label>
-              <select class="form-control" id="city" name="city">
-                <?php selectCity(); ?>
-            </select>
-            </div>
+            <div class="row ">
+                  <div class="form-group col-md-6">
+                    <label for="category"><span class="glyphicon glyphicon-bookmark"></span> Categoria:</label>
+                    <select class="form-control" id="category" name="category">
+                      <?php selectCates(); ?>
+                  </select>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="city"><span class="glyphicon glyphicon-bookmark"></span> Ciudad:</label>
+                    <select class="form-control" id="city" name="city">
+                      <?php selectCity(); ?>
+                  </select>
+                  </div>
+            </div> <!-- row -->
             <input type="submit" name="submit" id="submit" value="Publicar" class="center-block btn btn-warning">
         </form>
 
