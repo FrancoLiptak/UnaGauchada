e9db74237345aa58dff61d27628d6b85abbdd094
SebaRaimondi <sebastianraimondi@gmail.com>
2017-05-26 17:29:56 -0300

    Arreglado ver imagen

--- gauchadasFx.php
+++ gauchadasFx.php
@@ -34,16 +34,13 @@ function showGauchadaForAll($gauchada)
 
         ?>
         <tr> 
-            <td><img class="img-thumbnail"src="imgs/logoUnaGauchada.png"></td>
+            <td><img class="img-thumbnail"src="<?php if($gauchada['image'] == null) { echo "uploads/63229-logoUnaGauchada.png"; }else { echo $gauchada['image']; }?>"></td>
             <td><?php echo $user['name']; ?></td>
             <td><?php echo $cate['name']; ?></td>
             <td><?php echo $city['name']; ?></td>
             <td><?php echo $gauchada['title']; ?></td>
             <td><?php echo date_diff(date_create($gauchada['expiration']), date_create($hoy))->format('%a'); ?></td>
             <td><a class="details" href="detalle.php?idGauchadas=<?php echo $gauchada['idGauchadas']; ?>">&raquo; Ver detalle</a>
-
-    <img src="<?php echo $gauchada['image']; ?>" />
-
             </td> 
          </tr> 
     <?php  
