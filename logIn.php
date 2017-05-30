cf33b72fad65c5e3624189236d89c42577fb422f
SebaRaimondi <sebastianraimondi@gmail.com>
2017-05-30 19:57:29 -0300

    Arreglado por tercera vez lo de login

--- logIn.php
+++ logIn.php
@@ -1,65 +1,60 @@
-<html>
+<?php
+include_once "validate.php";
+if (validateLogin()) {
+    $_SESSION['msg'] = "No puede ingresar a logout.php si no tiene una sesion iniciada.";
+    header('Location: index.php');
+    die;
+}
+include_once "header.php";
+include_once "alert.php";
+?>
+  <html>
 
-<head>
-  <title>Log In</title>
-  <?php include_once "header.php";
-  include_once "alert.php";
-  include_once "validate.php";
-  
-    ?>
-  <div class="row">
-    <?php 
-     if (validateLogin()) { ?> <div class="logged-error"><?php 
-              hacerAlert("No puede ingresar a logIn.php si ya tiene una sesion iniciada.");
-              
-              ?>
-              </div>
-              <?php  
-              die;
-    }
-    ?>
-    <div class="container-fluid  col-md-4 col-md-offset-4">
-      <div class="page-header ">
-        <h4 style="text-align:center;"> Si aún no te has <a href="signUp.php">registrado</a>, no dudes en hacerlo. </h4>
+  <head>
+    <title>Log In</title>
+    <div class="row">
+      <div class="container-fluid  col-md-4 col-md-offset-4">
+        <div class="page-header ">
+          <h4 style="text-align:center;"> Si aún no te has <a href="signUp.php">registrado</a>, no dudes en hacerlo. </h4>
+        </div>
+        <br>
       </div>
-      <br>
-    </div>
-    <?php  /* por si ocurre algun error al loguearse */
-    if (isset($_SESSION['mal'])) {
-        hacerAlert($_SESSION['mal']);
-        unset($_SESSION['mal']);
-    }
+        <?php  /* por si ocurre algun error al loguearse */
+        if (isset($_SESSION['mal'])) {
+            hacerAlert($_SESSION['mal']);
+            unset($_SESSION['mal']);
+        }
    
-    if (isset($_SESSION['msg']) && $_SESSION['msg'] != "") {
-        hacerAlert($_SESSION['msg']);
-        $_SESSION['msg'] = "";
-    }
+        if (isset($_SESSION['msg']) && $_SESSION['msg'] != "") {
+            hacerAlert($_SESSION['msg']);
+            $_SESSION['msg'] = "";
+        }
     ?>
 
-    <form class="col-md-4 col-md-offset-4" action="procesarLogIn.php" method="post" target="_self" accept-charset="UTF-8" autocomplete="on"
-      name="logIn_form" onsubmit="return validateFormLogIn()">
-      <div class="form-group">
-        <label>Email:</label>
-        <input class="form-control" type="email" name="email" placeholder=" E-mail...">
+      <form class="col-md-4 col-md-offset-4" action="procesarLogIn.php" method="post" target="_self" accept-charset="UTF-8" autocomplete="on"
+        name="logIn_form" onsubmit="return validateFormLogIn()">
+        <div class="form-group">
+          <label>Email:</label>
+          <input class="form-control" type="email" name="email" placeholder=" E-mail...">
+        </div>
+        <div class="form-group">
+          <label>Contraseña:</label>
+          <input class="form-control" type="password" name="pass" placeholder=" Contraseña...">
+        </div>
+        <div class="form-group">
+          <input type="submit" name="submit" id="submit" value="Log in" class="center-block btn btn-warning">
+          <br><br>
+          <input class="center-block btn btn-info" id="submit" type="link" onclick="location.href='signUp.php';" value="Click aquí para registrarte"
+          />
+      </form>
       </div>
-      <div class="form-group">
-        <label>Contraseña:</label>
-        <input class="form-control" type="password" name="pass" placeholder=" Contraseña...">
-      </div>
-      <div class="form-group">
-        <input type="submit" name="submit" id="submit" value="Log in" class="center-block btn btn-warning">
-        <br><br>
-        <input class="center-block btn btn-info" id="submit" type="link" onclick="location.href='signUp.php';" value="Click aquí para registrarte"
-        />
-    </form>
-    </div>
-    <!-- Cierro row -->
+      <!-- Cierro row -->
 
-    <!-------------------------------------------- Script para validar el login -->
-    <script rel="text/javascript" src="js/logIn.js"></script>
+      <!-------------------------------------------- Script para validar el login -->
+      <script rel="text/javascript" src="js/logIn.js"></script>
 
-    <?php
-    include_once "footer.html"; ?>
-      </body>
+        <?php
+        include_once "footer.html"; ?>
+        </body>
 
-</html>
+  </html>
