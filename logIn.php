79992bd20b4f7bf53f89683a1e4143a8d08456d0
SebaRaimondi <sebastianraimondi@gmail.com>
2017-05-30 20:14:34 -0300

    Fix login

--- logIn.php
+++ logIn.php
@@ -1,7 +1,10 @@
 <?php
 include_once "validate.php";
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 if (validateLogin()) {
-    $_SESSION['msg'] = "No puede ingresar a logout.php si no tiene una sesion iniciada.";
+    $_SESSION['msg'] = "No puede ingresar a login.php si no tiene una sesion iniciada.";
     header('Location: index.php');
     die;
 }
