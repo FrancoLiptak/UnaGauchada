4240b9b7c36768684b50aca8f1b7fbd7fb3afa74
SebaRaimondi <sebastianraimondi@gmail.com>
2017-05-30 20:53:43 -0300

    Agregada funcion creditValue

--- credits.php
+++ credits.php
@@ -1,12 +1,12 @@
 <?php
 
-    include_once 'connect.php';
-    include_once 'validate.php';
-    include_once 'getUser.php';
+include_once 'connect.php';
+include_once 'validate.php';
+include_once 'getUser.php';
 
 function getCredits($idUser)
 {
-    // Devuelve los creditos del usuario con id = $idUser. Si hay un error en la consulta, devuelve false.
+// Devuelve los creditos del usuario con id = $idUser. Si hay un error en la consulta, devuelve false.
 
     $link = connect();
 
@@ -24,14 +24,14 @@ function getCredits($idUser)
 
 function validateCredits($idUser)
 {
-    // Devuelve true si la cantidad de creditos del usuario $idUser es >= 1.
+// Devuelve true si la cantidad de creditos del usuario $idUser es >= 1.
 
     return getCredits($idUser) >= 1;
 }
 
 function incrementCredits($idUser, $amount = 1)
 {
-    // Aumenta en $amount la cantidad de creditos del usuario con id = $idUser.
+// Aumenta en $amount la cantidad de creditos del usuario con id = $idUser.
 
     $credits = getCredits($idUser) + $amount;
     $link = connect();
@@ -41,7 +41,24 @@ function incrementCredits($idUser, $amount = 1)
 
 function decrementCredits($idUser, $amount = 1)
 {
-    // Decrementa en 1 la cantidad de creditos del usuario $idUser. Devuelve true si no hubo errores en el query.
+// Decrementa en 1 la cantidad de creditos del usuario $idUser. Devuelve true si no hubo errores en el query.
 
     return incrementCredits($idUser, $amount * -1);
+}
+
+function creditValue()
+{
+// Retorna el precio de los creditos o false en caso de error.
+    $link = connect();
+
+    $query = "SELECT * FROM credit WHERE idCredit = 1";
+    $result = $link->query($query);
+
+    if ($result) {
+        if ($row = $result->fetch_array()) {
+            return $row['price'];
+        }
+    }
+
+    return false;
 }
