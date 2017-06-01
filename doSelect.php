1889eea3eceeadc237801b191086ea82c7d200b4
SebaRaimondi <sebastianraimondi@gmail.com>
2017-05-31 21:23:54 -0300

    Provincia, ciudad

--- doSelect.php
+++ doSelect.php
@@ -19,7 +19,25 @@ function doSelect($table)
 }
 function selectCity()
 {
-    doSelect('city');
+    $link = connect();
+    $query = "SELECT * FROM city;";
+    $resultProv = "SELECT * FROM province;";
+    $resultProv = $link->query($resultProv);
+    while ($row = $resultProv->fetch_assoc()) {
+        $provArray[$row['idProvince']]['id'] = $row['idProvince'];
+        $provArray[$row['idProvince']]['name'] = $row['name'];
+    }
+    if ($result = $link->query($query)) {
+
+        while ($row = $result->fetch_assoc()) {
+            ?>
+
+            <option class="fondoGaucho" value="<?php echo($row['idCity']); ?>" > <?php echo $provArray[$row['idProvince']]['name'].", ".$row['name'] ; ?> </option>
+
+<?php
+        }
+    }
+    mysqli_close($link);
 }
 function selectCates()
 {
