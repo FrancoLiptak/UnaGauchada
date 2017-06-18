4e295152037c9cf8c4fd07fadfbff91cc331ae49
Unknown <franqoo.96@hotmail.com>
2017-06-18 19:47:49 -0300

    hecho perfil

--- perfil.php
+++ perfil.php
@@ -13,12 +13,99 @@ include_once 'alert.php';
 <html>
 <head>
     <title>Perfil</title>
-    <div class="row center-block">
-    	<?php
-            showUser(   mysqli_fetch_array(getUser($_SESSION['idUsers']))  );        //no hago ningun validate del idUsers porque lo hice arriba
-        ?>
+    <?php
+        include("footer.html");
+        getUser($_SESSION['idUsers']); //En una consulta traigo todos los datos
+    ?>
+</head>
 
-   	</div>
+<body>
 
-<br><br><br><br>
-<?php include("footer.html");?>
+
+<div class="perfil">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="container-fluid  col-md-6 col-md-offset-3 " >
+          <div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title">Información personal</h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-3 col-lg-3 " align="center"> 
+                    <img alt="User Pic" class="img-rounded img-responsive" src='<?php getPhoto(); ?>'>
+                </div>
+                <div class=" col-md-9 col-lg-9 "> 
+                  <table class="table table-user-information">
+                    <tbody>
+                      <tr>
+                        <td>Nombre:</td>
+                        <td>
+                            <?php
+                                getName();
+                            ?>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Apellido:</td>
+                        <td>
+                            <?php
+                                getSurname();
+                            ?>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Reputación:</td>
+                        <td>
+                            <?php
+                                getReputation();
+                            ?>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Créditos:</td>
+                        <td>
+                            <?php
+                                getCredits();
+                            ?>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Fecha de nacimiento:</td>
+                        <td>
+                            <?php
+                                getBirthDate();
+                            ?>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Teléfono:</td>
+                        <td>
+                            <?php
+                                getPhone();
+                            ?>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>E-mail</td>
+                        <td>
+                            <?php
+                                getEmail();
+                            ?>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  
+                </div>
+              </div>
+              <a class="btn btn-primary">Actualizar información</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+
+</body>
+</html>
--- usersFx.php
+++ usersFx.php
@@ -1,5 +1,7 @@
 <?php
 
+    $user;
+
     function getUser($idUser) {
         $link = connect();
 		$query = "SELECT * FROM users WHERE idUsers = $idUser;";
@@ -8,42 +10,47 @@
             printf("Errormessage: %s\n", $link->error);
             return false;
         }
-		return $result;
+		global $user;
+        $user = mysqli_fetch_array($result);
 	}
 
-    function showUser($user) {?>
-        
-    <div class="container col-md-12 centered gauchadaBox ph">
-        <div class="page-header">
-            <h2>
-                <?php echo $user['name'],' ',$user['surname'] ; ?>
-            </h2>
-        </div>
-        <div class="col-md-6">
-            <img class="img-thumbnail img-detail" src="
-                <?php
-                if ($user['photo'] == null) {
-                echo " uploads/nophoto.png ";
-                } else {
-                echo $user['photo'];
-                }
-                ?> 
-                "> <br><br>
-        <a href="" class="btn btn-info">Editar Perfil</a>
-        </div>
-        <div class="col-md-6">
-            <?php echo $user['birthDate']; ?>
-            <?php echo $user['credits']; ?>
-            <?php echo $user['email']; ?>
-            <?php echo $user['phone']; ?>
-            <?php echo $user['reputation']; ?>
-            <?php echo $user['birthDate']; ?>
-        </div>
+    function getName(){
+        global $user;
+        echo $user['name'];
+    }
 
+    function getSurname(){
+        global $user;
+        echo $user['surname'];
+    }
 
+    function getEmail(){
+        global $user;
+        echo $user['email'];
+    }
 
+    function getBirthDate(){
+        global $user;
+        echo $user['birthDate'];
+    }
 
-    </div>
+    function getPhone(){
+        global $user;
+        echo $user['phone'];
+    }
 
-    <?php }
+    function getReputation(){
+        global $user;
+        echo $user['reputation'];
+    }
+
+    function getCredits(){
+        global $user;
+        echo $user['credits'];
+    }
+
+    function getPhoto(){
+        global $user;
+        echo $user['photo'];
+    }
 ?>
