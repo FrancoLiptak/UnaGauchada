a08f362867bf45c163b5fa939edf8d18595ff848
Camila <onofricamila2@gmail.com>
2017-06-26 20:39:09 -0300

    ni idea

--- miCuenta.php
+++ miCuenta.php
@@ -10,12 +10,12 @@
         } else {
                 ?>
             <li><a target="_self" href="comprarCreditos.php"><span class="glyphicon glyphicon-credit-card"></span> Comprar creditos </a></li>
+            <li><a target="_self" href="helped.php"><span class="glyphicon glyphicon-thumbs-up"></span> Mis postulaciones </a></li>
             <li><a target="_self" href="misGauchadas.php"><span class="glyphicon glyphicon-triangle-right"></span> Mis gauchadas</a></li>
                 <?php
         }
 ?>
         <li><a target="_self" href="perfil.php"><span class="glyphicon glyphicon-user"></span> Ver perfil </a></li>
-        <li><a target="_self" href="helped.php"><span class="glyphicon glyphicon-thumbs-up"></span> Mis postulaciones </a></li>
         <li role="separator" class="divider"></li>
         <li><a target="_self" href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Cerrar sesion </a></a></li>
     </ul>
