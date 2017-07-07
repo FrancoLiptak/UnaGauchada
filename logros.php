43beecf1db234032e4bd8d42cbc83e19580e3083
Camila <onofricamila2@gmail.com>
2017-07-06 21:19:11 -0300

    test slider

---
+++ logros.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <?php
+        include_once "header.php";
+        include_once 'validate.php';
+        include_once 'gauchadasFx.php';
+        include_once 'alert.php';
+        include("footer.html");
+        ?>
+    </head>
+    <body>
+        <div class="row">
+            <div class="container-fluid  col-md-4 col-md-offset-4 ph">
+                <div class="page-header">
+                    <h3 style="text-align:center;">Logros</h3>
+                </div>
+            </div>
+        </div>
+        <br>
+
+       
