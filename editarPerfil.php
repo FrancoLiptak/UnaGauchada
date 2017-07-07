0d7fb35e35ef875e0e3bf5513c79b36eb90ca80d
Camila <onofricamila2@gmail.com>
2017-07-06 22:13:33 -0300

    toonee eso

--- editarPerfil.php
+++ editarPerfil.php
@@ -21,82 +21,82 @@
 
         <script type="text/javascript">
             $(document).ready( function() {
-        $(document).on('change', '.btn-file :file', function() {
-        var input = $(this),
-            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-        input.trigger('fileselect', [label]);
-        });
+                $(document).on('change', '.btn-file :file', function() {
+                var input = $(this),
+                    label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+                input.trigger('fileselect', [label]);
+                });
 
-        $('.btn-file :file').on('fileselect', function(event, label) {
-            
-            var input = $(this).parents('.input-group').find(':text'),
-                log = label;
-            
-            if( input.length ) {
-                input.val(log);
-            } else {
-                if( log ) alert(log);
-            }
-        
-        });
-        function readURL(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
+                $('.btn-file :file').on('fileselect', function(event, label) {
+                    
+                    var input = $(this).parents('.input-group').find(':text'),
+                        log = label;
+                    
+                    if( input.length ) {
+                        input.val(log);
+                    } else {
+                        if( log ) alert(log);
+                    }
                 
-                reader.onload = function (e) {
-                    $('#img-upload').attr('src', e.target.result);
+                });
+                function readURL(input) {
+                    if (input.files && input.files[0]) {
+                        var reader = new FileReader();
+                        
+                        reader.onload = function (e) {
+                            $('#img-upload').attr('src', e.target.result);
+                        }
+                        
+                        reader.readAsDataURL(input.files[0]);
+                    }
                 }
-                
-                reader.readAsDataURL(input.files[0]);
-            }
-        }
 
-        $("#imgInp").change(function(){
-            readURL(this);
-        });     
-    });
+                $("#imgInp").change(function(){
+                    readURL(this);
+                });     
+            });
         </script>
 
         <style type="text/css">
             .btn-file {
-    position: relative;
-    overflow: hidden;
-}
-.btn-file input[type=file] {
-    position: absolute;
-    top: 0;
-    right: 0;
-    min-width: 100%;
-    min-height: 100%;
-    font-size: 100px;
-    text-align: right;
-    filter: alpha(opacity=0);
-    opacity: 0;
-    outline: none;
-    background: white;
-    cursor: inherit;
-    display: block;
-}
+                position: relative;
+                overflow: hidden;
+            }
+            .btn-file input[type=file] {
+                position: absolute;
+                top: 0;
+                right: 0;
+                min-width: 100%;
+                min-height: 100%;
+                font-size: 100px;
+                text-align: right;
+                filter: alpha(opacity=0);
+                opacity: 0;
+                outline: none;
+                background: white;
+                cursor: inherit;
+                display: block;
+            }
 
-#img-upload{
-    width: 100%;
-}
-        </style>
+            #img-upload{
+                width: 100%;
+            }
+                    </style>
 
 
         <div class="container">
-<div class="col-md-6">
-    <div class="form-group">
-        <label>Upload Image</label>
-        <div class="input-group">
-            <span class="input-group-btn">
-                <span class="btn btn-default btn-file">
-                    Browse… <input type="file" id="imgInp">
-                </span>
-            </span>
-            <input type="text" class="form-control" readonly>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label>Imagen de perfil</label>
+                    <div class="input-group">
+                        <span class="input-group-btn">
+                            <span class="btn btn-default btn-file">
+                                Examinar... <input type="file" id="imgInp">
+                            </span>
+                        </span>
+                        <input type="text" class="form-control" readonly>
+                    </div>
+                    <img id='img-upload' src="imgs/at1.jpg"/>
+                </div>
+            </div>
         </div>
-        <img id='img-upload'/>
-    </div>
-</div>
-</div>
--- logros.php
+++ logros.php
@@ -19,55 +19,18 @@
         <br>
 
            <div class="container">
-      <h2>Range Sliders <small>Only tested in Chrome!</small></h2>
-      <hr/>
+              <h2>Range Sliders <small>Elige el mínimo de reputación para adquirir el nuevo logro!</small></h2>
+              <hr/>
 
-      <div class="row">
-        <div class="col-xs-6">
-          <div class="range">
-            <input type="range" name="range" min="1" max="100" value="50" onchange="range.value=value">
-            <output id="range">50</output>
-          </div>
-        </div>
-        <div class="col-xs-6">
-          <div class="range range-primary">
-            <input type="range" name="range" min="1" max="100" value="50" onchange="rangePrimary.value=value">
-            <output id="rangePrimary">50</output>
-          </div>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-xs-6">
-          <div class="range range-success">
-            <input type="range" name="range" min="1" max="100" value="50" onchange="rangeSuccess.value=value">
-            <output id="rangeSuccess">50</output>
-          </div>
-        </div>
-        <div class="col-xs-6">
-          <div class="range range-info">
-            <input type="range" name="range" min="1" max="100" value="50" onchange="rangeInfo.value=value">
-            <output id="rangeInfo">50</output>
-          </div>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-xs-6">
-          <div class="range range-warning">
-            <input type="range" name="range" min="1" max="100" value="50" onchange="rangeWarning.value=value">
-            <output id="rangeWarning">50</output>
-          </div>
-        </div>
-        <div class="col-xs-6">
-          <div class="range range-danger">
-            <input type="range" name="range" min="1" max="100" value="50" onchange="rangeDanger.value=value">
-            <output id="rangeDanger">50</output>
-          </div>
-        </div>
-      </div>
-
-    </div>
+              <div class="row">
+                <div class="col-xs-6">
+                  <div class="range range-warning">
+                    <input type="range" name="range" min="1" max="100" value="50" onchange="rangeWarning.value=value">
+                    <output id="rangeWarning">50 /output>
+                  </div>
+                </div>
+                
+            </div>
 
 
 
@@ -75,161 +38,161 @@
     <style type="text/css">
 
 
-.range {
-    display: table;
-    position: relative;
-    height: 25px;
-    margin-top: 20px;
-    background-color: rgb(245, 245, 245);
-    border-radius: 4px;
-    -webkit-box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.1);
-    box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.1);
-    cursor: pointer;
-}
+            .range {
+                display: table;
+                position: relative;
+                height: 25px;
+                margin-top: 20px;
+                background-color: rgb(245, 245, 245);
+                border-radius: 4px;
+                -webkit-box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.1);
+                box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.1);
+                cursor: pointer;
+            }
 
-.range input[type="range"] {
-    -webkit-appearance: none !important;
-    -moz-appearance: none !important;
-    -ms-appearance: none !important;
-    -o-appearance: none !important;
-    appearance: none !important;
+            .range input[type="range"] {
+                -webkit-appearance: none !important;
+                -moz-appearance: none !important;
+                -ms-appearance: none !important;
+                -o-appearance: none !important;
+                appearance: none !important;
 
-    display: table-cell;
-    width: 100%;
-    background-color: transparent;
-    height: 25px;
-    cursor: pointer;
-}
-.range input[type="range"]::-webkit-slider-thumb {
-    -webkit-appearance: none !important;
-    -moz-appearance: none !important;
-    -ms-appearance: none !important;
-    -o-appearance: none !important;
-    appearance: none !important;
+                display: table-cell;
+                width: 100%;
+                background-color: transparent;
+                height: 25px;
+                cursor: pointer;
+            }
+            .range input[type="range"]::-webkit-slider-thumb {
+                -webkit-appearance: none !important;
+                -moz-appearance: none !important;
+                -ms-appearance: none !important;
+                -o-appearance: none !important;
+                appearance: none !important;
 
-    width: 11px;
-    height: 25px;
-    color: rgb(255, 255, 255);
-    text-align: center;
-    white-space: nowrap;
-    vertical-align: baseline;
-    border-radius: 0px;
-    background-color: rgb(153, 153, 153);
-}
+                width: 11px;
+                height: 25px;
+                color: rgb(255, 255, 255);
+                text-align: center;
+                white-space: nowrap;
+                vertical-align: baseline;
+                border-radius: 0px;
+                background-color: rgb(153, 153, 153);
+            }
 
-.range input[type="range"]::-moz-slider-thumb {
-    -webkit-appearance: none !important;
-    -moz-appearance: none !important;
-    -ms-appearance: none !important;
-    -o-appearance: none !important;
-    appearance: none !important;
-    
-    width: 11px;
-    height: 25px;
-    color: rgb(255, 255, 255);
-    text-align: center;
-    white-space: nowrap;
-    vertical-align: baseline;
-    border-radius: 0px;
-    background-color: rgb(153, 153, 153);
-}
+            .range input[type="range"]::-moz-slider-thumb {
+                -webkit-appearance: none !important;
+                -moz-appearance: none !important;
+                -ms-appearance: none !important;
+                -o-appearance: none !important;
+                appearance: none !important;
+                
+                width: 11px;
+                height: 25px;
+                color: rgb(255, 255, 255);
+                text-align: center;
+                white-space: nowrap;
+                vertical-align: baseline;
+                border-radius: 0px;
+                background-color: rgb(153, 153, 153);
+            }
 
-.range output {
-    display: table-cell;
-    padding: 3px 5px 2px;
-    min-width: 40px;
-    color: rgb(255, 255, 255);
-    background-color: rgb(153, 153, 153);
-    text-align: center;
-    text-decoration: none;
-    border-radius: 4px;
-    border-bottom-left-radius: 0;
-    border-top-left-radius: 0;
-    width: 1%;
-    white-space: nowrap;
-    vertical-align: middle;
+            .range output {
+                display: table-cell;
+                padding: 3px 5px 2px;
+                min-width: 40px;
+                color: rgb(255, 255, 255);
+                background-color: rgb(153, 153, 153);
+                text-align: center;
+                text-decoration: none;
+                border-radius: 4px;
+                border-bottom-left-radius: 0;
+                border-top-left-radius: 0;
+                width: 1%;
+                white-space: nowrap;
+                vertical-align: middle;
 
-    -webkit-transition: all 0.5s ease;
-    -moz-transition: all 0.5s ease;
-    -o-transition: all 0.5s ease;
-    -ms-transition: all 0.5s ease;
-    transition: all 0.5s ease;
+                -webkit-transition: all 0.5s ease;
+                -moz-transition: all 0.5s ease;
+                -o-transition: all 0.5s ease;
+                -ms-transition: all 0.5s ease;
+                transition: all 0.5s ease;
 
-    -webkit-user-select: none;
-    -khtml-user-select: none;
-    -moz-user-select: -moz-none;
-    -o-user-select: none;
-    user-select: none;
-}
-.range input[type="range"] {
-    outline: none;
-}
+                -webkit-user-select: none;
+                -khtml-user-select: none;
+                -moz-user-select: -moz-none;
+                -o-user-select: none;
+                user-select: none;
+            }
+            .range input[type="range"] {
+                outline: none;
+            }
 
-.range.range-primary input[type="range"]::-webkit-slider-thumb {
-    background-color: rgb(66, 139, 202);
-}
-.range.range-primary input[type="range"]::-moz-slider-thumb {
-    background-color: rgb(66, 139, 202);
-}
-.range.range-primary output {
-    background-color: rgb(66, 139, 202);
-}
-.range.range-primary input[type="range"] {
-    outline-color: rgb(66, 139, 202);
-}
+            .range.range-primary input[type="range"]::-webkit-slider-thumb {
+                background-color: rgb(66, 139, 202);
+            }
+            .range.range-primary input[type="range"]::-moz-slider-thumb {
+                background-color: rgb(66, 139, 202);
+            }
+            .range.range-primary output {
+                background-color: rgb(66, 139, 202);
+            }
+            .range.range-primary input[type="range"] {
+                outline-color: rgb(66, 139, 202);
+            }
 
-.range.range-success input[type="range"]::-webkit-slider-thumb {
-    background-color: rgb(92, 184, 92);
-}
-.range.range-success input[type="range"]::-moz-slider-thumb {
-    background-color: rgb(92, 184, 92);
-}
-.range.range-success output {
-    background-color: rgb(92, 184, 92);
-}
-.range.range-success input[type="range"] {
-    outline-color: rgb(92, 184, 92);
-}
+            .range.range-success input[type="range"]::-webkit-slider-thumb {
+                background-color: rgb(92, 184, 92);
+            }
+            .range.range-success input[type="range"]::-moz-slider-thumb {
+                background-color: rgb(92, 184, 92);
+            }
+            .range.range-success output {
+                background-color: rgb(92, 184, 92);
+            }
+            .range.range-success input[type="range"] {
+                outline-color: rgb(92, 184, 92);
+            }
 
-.range.range-info input[type="range"]::-webkit-slider-thumb {
-    background-color: rgb(91, 192, 222);
-}
-.range.range-info input[type="range"]::-moz-slider-thumb {
-    background-color: rgb(91, 192, 222);
-}
-.range.range-info output {
-    background-color: rgb(91, 192, 222);
-}
-.range.range-info input[type="range"] {
-    outline-color: rgb(91, 192, 222);
-}
+            .range.range-info input[type="range"]::-webkit-slider-thumb {
+                background-color: rgb(91, 192, 222);
+            }
+            .range.range-info input[type="range"]::-moz-slider-thumb {
+                background-color: rgb(91, 192, 222);
+            }
+            .range.range-info output {
+                background-color: rgb(91, 192, 222);
+            }
+            .range.range-info input[type="range"] {
+                outline-color: rgb(91, 192, 222);
+            }
 
-.range.range-warning input[type="range"]::-webkit-slider-thumb {
-    background-color: rgb(240, 173, 78);
-}
-.range.range-warning input[type="range"]::-moz-slider-thumb {
-    background-color: rgb(240, 173, 78);
-}
-.range.range-warning output {
-    background-color: rgb(240, 173, 78);
-}
-.range.range-warning input[type="range"] {
-    outline-color: rgb(240, 173, 78);
-}
+            .range.range-warning input[type="range"]::-webkit-slider-thumb {
+                background-color: rgb(240, 173, 78);
+            }
+            .range.range-warning input[type="range"]::-moz-slider-thumb {
+                background-color: rgb(240, 173, 78);
+            }
+            .range.range-warning output {
+                background-color: rgb(240, 173, 78);
+            }
+            .range.range-warning input[type="range"] {
+                outline-color: rgb(240, 173, 78);
+            }
 
-.range.range-danger input[type="range"]::-webkit-slider-thumb {
-    background-color: rgb(217, 83, 79);
-}
-.range.range-danger input[type="range"]::-moz-slider-thumb {
-    background-color: rgb(217, 83, 79);
-}
-.range.range-danger output {
-    background-color: rgb(217, 83, 79);}
+            .range.range-danger input[type="range"]::-webkit-slider-thumb {
+                background-color: rgb(217, 83, 79);
+            }
+            .range.range-danger input[type="range"]::-moz-slider-thumb {
+                background-color: rgb(217, 83, 79);
+            }
+            .range.range-danger output {
+                background-color: rgb(217, 83, 79);}
 
     </style>
 
     <script type="text/javascript">
         $('input[name="range"]').on("change mousemove", function() {
-$(this).next().html($(this).val() + '%');
-});
+        $(this).next().html($(this).val() );
+        });
     </script>
