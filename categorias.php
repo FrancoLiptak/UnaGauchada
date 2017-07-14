4c7c715cc41f84e14cc484122b4162f2bc315335
Camila <onofricamila2@gmail.com>
2017-07-13 22:52:10 -0300

    eliminar cate pasaje de parametros al pop up

--- categorias.php
+++ categorias.php
@@ -3,6 +3,16 @@
 <head>
   <title>Categorias</title>
   <?php
+  /* 
+  sebas: para hacer codigo php para borrar/editar una cate anda al form con submit 'Si, deseo eliminar' para ver como se manda el idCategory en un input hidden. 
+
+  READ ME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  Voy a explicar el funcionamiento del borrar/editar cates.
+  Primero, estamos usando pop ups (mas precisamente divs de la clase modal) y se complico un poco el pasaje de parametros a estos. Lo que hago es que, al hacer click en el boton que abre el modal para editar/borrar la cate particular, disparar una funcion js (que esta en este archivo abajo de todo) que lo que hace es asignar el data-id del boton presionado (que va a ser la concatenacion: id/nombre de la categoria a la que esta asociado el boton) al elemento html dentro del body del modal que tenga el id que yo le digo en la funcion (ver abajo).
+  NOTAS:
+    -Es importante notar los botones para elimnar y editar deben tener el att data-id="<datos de la cate>".
+    -Como en la funcion js lo que hago es cambiar el value de un elemento dentro del modal-body, ese elemento debe ser un input, li, option entre otros; es decir, algun elemento html que tenga el att value. De todos los elementos que tienen value me sirve el input, es por eso que el nombre de la categoria se ve en un input text disabled, y necesito crear un form para que se envie el id de la categoria en un input hidden idCategory al apretar 'Si, acepto'. ESTO ES IMPORTANTE PORQUE RECIEN ACA ES CUANDO SE MANDA A BORRAR!!!!! Se procesaria ese form en un eliminarCate.php.
+  */
   include_once "validate.php";
   if (session_status() == PHP_SESSION_NONE) {
     session_start();
@@ -42,7 +52,7 @@
                     <a href="editarCategoria.php?id=<?php echo $cate['idCategory']; ?>" role="button" class="btn btn-default btn-sm" >
                       <i class="fa fa-pencil-square-o"></i>
                     </a>
-                    <a href="eliminarCategoria.php?id=<?php echo $cate['idCategory']; ?>" role="button" class="btn btn-default btn-sm" >
+                    <a data-toggle="modal" data-target="#deleteModal" role="button" class="btn btn-default btn-sm openDeleteModal" data-id="<?php echo $cate['idCategory'].'/'.$cate['name']; ?>" id="deleteButton" href="#!">
                       <i class="fa fa-trash "></i>
                     </a>
                   </span>
@@ -55,40 +65,75 @@
   </div> <!-- fin del div que contiene a la tabla de cates -->
 
 
-<!-- line modal -->
-<div class="modal fade" id="squarespaceModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-  <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
-        <h3 class="modal-title" id="lineModalLabel">Añade una nueva categoría!</h3>
-      </div>
-      <div class="modal-body">
-           <!-- content goes here -->
-              <form class="" action="procesarCate.php" method="post" target="_self" accept-charset="UTF-8" autocomplete="on"
-                name="formCate">
-                <div class="form-group col-md-12">
-                  <label><span class="glyphicon glyphicon-bookmark"></span> Nombre:</label>
-                  <input class="form-control" type="text" name="cate" placeholder=" Categoria..." required>
-                </div>
-                <div class="form-group col-md-6">
-                  <input type="submit" name="submit" id="submit" value="Crear" class=" btn btn-warning col-md-6">
-                </div>
-                <div class="form-group col-md-6">
-                  <button type="button" id="submit" class="btn btn-danger" data-dismiss="modal"  role="button">Cancelar</button>
-                </div>
-              </form> <!-- fin del form -->
-      </div>
-      <div class="modal-footer" style="border:none;">
-        
-      </div>
+  <!-- add cat modal -->
+  <div class="modal fade" id="squarespaceModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+          <h3 class="modal-title" id="lineModalLabel">Añade una nueva categoría!</h3>
+        </div>
+        <div class="modal-body">
+             <!-- content goes here -->
+                <form class="" action="procesarCate.php" method="post" target="_self" accept-charset="UTF-8" autocomplete="on" name="formCate">
+                  <div class="form-group col-md-12">
+                    <label><span class="glyphicon glyphicon-bookmark"></span> Nombre:</label>
+                    <input class="form-control" type="text" name="cate" placeholder=" Categoria..." required>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <input type="submit" name="submit" id="submit" value="Crear" class=" btn btn-warning col-md-6">
+                  </div>
+                  <div class="form-group col-md-6">
+                    <button type="button" id="submit" class="btn btn-danger" data-dismiss="modal"  role="button">Cancelar</button>
+                  </div>
+                </form> <!-- fin del form -->
+        </div>
+        <div class="modal-footer" style="border:none;">
+          
+        </div>
+    </div>
+    </div>
   </div>
-  </div>
-</div>
 
-  <!-- 
+  <!-- delete modal -->
+  <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+          <h3 class="modal-title" id="lineModalLabel">¡Atencion!</h3>
+        </div>
+        <div class="modal-body">
+             <!-- content goes here -->
+                <p>Estas seguro que deseas eliminar la categoria <input type="text" class="form-control" style="display:inline; width: 20%;" id="nameCategory" disabled>?</p>
+        </div>
+        <div class="modal-footer" style="border:none;">
+          <div class="col-md-6">
+            <button  id="submit" style="float: left;" type="reset" class="btn btn-danger" data-dismiss="modal">Cancelar eliminacion</button>
+          </div>
+          <form class="form-inline col-md-6" action="eliminarCate.php" method="post" target="_self" accept-charset="UTF-8" autocomplete="on" name="deleteCateForm">
+            <input type="number" id="idCategory" name="idCategory" hidden>
+            <button type="submit" id="submit" class="btn btn-primary">Si, deseo eliminar</button>
+          </form>
+        </div>
+    </div>
+    </div>
+  </div>
+
+
 
 </div> <!-- fin del row -->
+
+<script type="text/javascript">
+/* data('id') me trae el valor del att data-id del boton clickeado, que es: id/name de la categoria en cuestion. Con la funcion split lo que hago es crear un arreglo que separa sus elementos por el delimitador "/". Al abrir el modal le asigna el valor de ese arreglo creado en la pos 0 (corresponde al id de la categoria) a cualquier elemento html dentro del modal-body con id="idCategory" y el valor de ese arreglo creado en la pos 1 (corresponde al nombre de la categoria) a cualquier elemento html dentro del modal-body con id="nameCategory".
+*/
+
+$(document).on("click", ".openDeleteModal", function () {
+var idAndName = $(this).data('id').split("/", 2);
+$(".modal-body #nameCategory").val( idAndName[1] );
+$(".modal-body #idCategory").val( idAndName[0] );
+});
+</script>
 
 <?php include_once "footer.html"; ?>
 </body>
