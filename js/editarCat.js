a87820740dc7511165b73d53359237e3c71d22a6
SebaRaimondi <sebastianraimondi@gmail.com>
2017-07-18 14:57:29 -0300

    Odio esto

--- js/editarCat.js
+++
@@ -1,8 +0,0 @@
-function validateFormEditarCat() {
-	var nombre = document.editarCat_form.nombre.value;
-    if (nombre == null || nombre.length == 0 || /^\s*$/.test(nombre)){
-        alert("El campo 'nombre' no puede estar vacio o contener sólo espacios en blanco");
-        return false;
-    }
-     return true;
-}
