ada02def487295ac4e23fae716802c8920c79bdd
SebaRaimondi <sebastianraimondi@gmail.com>
2017-07-21 23:22:20 -0300

    Funcionalidades de categorias.

--- fxCategory.php
+++ fxCategory.php
@@ -23,4 +23,51 @@
         return false;
     }
 
+function newCategory($name){
+    $name = trim($name);
+    if (!validate($name)) {
+        return false;
+    }
+    $link = connect();
+    $query = "INSERT INTO category (name)";
+    $query = $query."VALUES ('$name')";
+    if ($result = $link->query($query)) {
+        $_SESSION['msg'] = "La categoria $name se creo correctamente.";
+        return $result;
+    }
+    $_SESSION['msg'] = $link->error;
+}
+
+function cateExists($name) {
+    $link = connect();
+    $query = "SELECT name FROM category WHERE name = '$name'";
+    if ($result = $link->query($query)) {
+        return $result->num_rows == 1;
+    }
+    $_SESSION['msg'] = $link->error;
+    return false;
+}
+
+function deleteCategory($id){
+    $link = connect();
+    $query = "UPDATE category SET deleted=1 WHERE idCategory=$id;";
+    if ($result = $link->query($query)) {
+        $_SESSION['msg'] = "La categoria $id se elimino correctamente.";
+        return $result;
+    }
+    $_SESSION['msg'] = $link->error;
+    return false;
+}
+
+function editCategory($id, $name){
+    $link = connect();
+    $query = "UPDATE category SET name='$name' WHERE idCategory=$id;";
+    if ($result = $link->query($query)) {
+        $_SESSION['msg'] = "La categoria $id se modifico correctamente. Nuevo nombre: $name.";
+        return $result;
+    }
+    $_SESSION['msg'] = $link->error;
+    return false;
+}
+
 ?>
---
+++ procesarCate.php
@@ -0,0 +1,38 @@
+<?php
+
+function toIndex() {
+    header('Location: index.php');
+    die;
+}
+function toCate() {
+    header('Location: categorias.php');
+    die;
+}
+
+include_once 'validate.php';
+
+if (!validateLogin()) {
+    $_SESSION['msg'] = "Debe estar logueado para realizar esta acción.";
+    toIndex();
+}
+
+if (!isAdmin()) {
+    $_SESSION['msg'] = "Solo los administradores pueden realizar esta accion.";
+    toIndex();
+}
+
+if (!isset($_POST['cate'])) {
+    $_SESSION['msg'] = "No se envio ningun nombre de categoria a crear.";
+    toCate();
+}
+
+$name = trim($_POST['cate']);
+include_once 'fxCategory.php';
+
+if (cateExists($name)) {
+    $_SESSION['msg'] = "El nombre de categoria ingresado ya esta en uso.";
+    toCate();
+}
+
+newCategory($name);
+toCate();
