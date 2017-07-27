6b58bb0c52b8dbf67756fffad2d7c098eac3518b
Unknown <sebastianraimondi@gmail.com>
2017-07-26 21:07:12 -0300

    1

--- fxCompras.php
+++ fxCompras.php
@@ -1,9 +1,8 @@
 <?php
 
-include_once "connect.php";
-include_once "usersFx.php";
-
 function getVentas($end = null, $start = null) {
+	include_once "connect.php";
+	include_once "usersFx.php";
 
 	if (is_null($start)) {
 		if (is_null($end)) {
@@ -14,23 +13,22 @@ function getVentas($end = null, $start = null) {
 	
 	$link = connect();
 	$query = "SELECT * FROM purchases WHERE date > '$start' AND date < '$end'";
-	if ($result = $link->query($query)) {
-		return $result;
+	if (!$result = $link->query($query)) {
+		$_SESSION['msg'] = $link->error;
+		return false;
 	}
-	$_SESSION['msg'] = $link->error;
-	return false;
 
-}
+	$ventas = array();
+	while ($current = $result->fetch_assoc()) {
+		$user = getUser($current['idUser'])->fetch_assoc();
+		$venta['nombre'] = $user['name'];
+		$venta['apellido'] = $user['surname'];
+		$venta['cantidad'] = $current['quantity'];
+		$venta['monto'] = $current['quantity'] * $current['price'];
+		$ventas[] = $venta;
+	}
 
-$result = getVentas();
-$ventas = array();
-while ($current = $result->fetch_assoc()) {
-	$user = getUser($current['idUser'])->fetch_assoc();
-	$venta['nombre'] = $user['name'];
-	$venta['apellido'] = $user['surname'];
-	$venta['cantidad'] = $current['quantity'];
-	$venta['monto'] = $current['quantity'] * $current['price'];
-	$ventas[] = $venta;
+	return $ventas;
 }
 
 ?>
