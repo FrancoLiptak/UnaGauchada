a85fb219d7c33299597b2a620dc82736ef340744
Camila <onofricamila2@gmail.com>
2017-08-02 15:53:03 -0300

    cambie redireccion al borrar gauchada para que vaya directamente al inicio

--- procesarEliminarGauchada.php
+++ procesarEliminarGauchada.php
@@ -1,13 +1,22 @@
 <?php
 
-include_once 'header.php';
 include_once 'alert.php';
 include_once 'gauchadasFx.php';
 include("footer.html");
 
-?> 
-    <br><br><br><br> 
-<?php
+if(deleteGauchada($_GET["id"])){
+    $_SESSION['success'] = "La gauchada se ha eliminado con éxito.";
+}else{
+    $_SESSION['msg'] = "La gauchada no pudo ser eliminada. Por favor, intente mas tarde.";
+
+}
+
+header('Location: index.php');
+
+
+/*
+
+echo "<br><br><br><br><br>";
 
 if(deleteGauchada($_GET["id"])){
     hacerAlert("La gauchada se ha eliminado con éxito.", "success");
@@ -18,6 +27,7 @@ if(deleteGauchada($_GET["id"])){
 
 echo "<center><a type='button' class='btn btn-default btn-filter' href='index.php'>Volver al inicio</a></center>";
 
+*/
 
 ?>
 
